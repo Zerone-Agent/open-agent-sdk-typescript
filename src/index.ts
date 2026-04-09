cd4e4d700e7b00d9e2a2b6f1b435276d51fd4f27
zero <tohhiyin@gmail.com>
2026-04-09 17:43:02 +0800

    feat: export StreamChunk type

--- src/index.ts
+++ src/index.ts
@@ -69,6 +69,7 @@ export type {
   NormalizedContentBlock,
   NormalizedTool,
   NormalizedResponseBlock,
+  StreamChunk,
 } from './providers/index.js'
 
 // --------------------------------------------------------------------------
